c3d52f11cffff78d778adcb5f49119a94d6e53ab
Prayag Bhakar <prayag.bhakar@gmail.com>
2020-11-09 22:02:31 -0600

    added Clean Beats

---
+++ Clean Beats/cleanbeatspreferences/CBSRootListController.m
@@ -0,0 +1,13 @@
+#include "CBSRootListController.h"
+
+@implementation CBSRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+	}
+
+	return _specifiers;
+}
+
+@end
